bf625264d0bbeebc5b68f4d5553382a55a9498df
RononDex <tinoheuberger@protonmail.com>
2021-08-08 16:56:34 +0200

    Fix missing ";"

--- Objects/DiscordGuild.cs
+++ Objects/DiscordGuild.cs
@@ -82,7 +82,7 @@ namespace AwesomeChatBot.Discord.Objects
 
         public override async Task<IList<User>> GetUserseOnServer()
         {
-            await Guild.DownloadUsersAsync()
+            await Guild.DownloadUsersAsync();
             return Guild.Users.Select(u => new DiscordUser(ApiWrapper, u) as User).ToList();
         }
     }
